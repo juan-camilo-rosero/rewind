eb3fa52da988022606826d9f0c3994b065b352fa
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-12 11:30:11 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,55 @@
-import { PiecesContextProvider } from './context/PiecesContext'
+import { PiecesContext, PiecesContextProvider } from './context/PiecesContext'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import Main from './components/Main'
 import SignUp from './components/SignUp'
-import { auth } from './firebase/firebase.config'
+import { auth, db } from './firebase/firebase.config'
 import './App.css'
 import { useContext, useEffect } from 'react'
 import { SectionContext } from './context/SectionContext'
-import { AuthContext } from './context/AuthContext'
+import { doc, getDoc } from 'firebase/firestore'
+import Loading from './components/Loading'
 
 function App() {
 
-  const {main, setMain} = useContext(SectionContext)
-  const {setUserId} = useContext(AuthContext)
+  const {main, setMain, loading, setLoading, setSection} = useContext(SectionContext)
+  const {newPiece, setActualPiece, updatePieces} = useContext(PiecesContext)
 
   useEffect(() => {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    const userCode = urlParams.get('code')
+    const piece = urlParams.get('piece')
     auth.onAuthStateChanged(async user => {
       if (!user) {
         setMain(false)
       }
       else{
-        setUserId(user.uid)
+        const userRef = doc(db, "users", user.email)
+            const userSnap = await getDoc(userRef)
+            if (userSnap.exists()) {
+                const userData = userSnap.data();
+                setActualPiece(userData.actual_piece)
+                const pieces = await updatePieces((userData.actual_piece == 0) ? 0 : userData.actual_piece - 1)
+                if(piece) setSection("art_piece")
+                if(userCode) await newPiece(userCode, userData.actual_piece, pieces, user.email)
+                setLoading(false)
+            } else {
+                console.log("No such document!");
+            }
       }
     });
   }, [])
 
   return (
     <>
-    
-    <PiecesContextProvider>
       <Header/>
       {(main)
-      ? <><Nav/><Main/></>
+      ? (loading) 
+        ? <Loading/>
+        : <><Nav/><Main/></>
       : <SignUp/>
       }
-    </PiecesContextProvider>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { SectionContextProvider } from './context/SectionContext'
 import { AuthContextProvider } from './context/AuthContext'
+import { PiecesContextProvider } from './context/PiecesContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthContextProvider>
     <SectionContextProvider>
+    <PiecesContextProvider>
       <App />
+    </PiecesContextProvider>
     </SectionContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
